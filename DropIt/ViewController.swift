8838effc6caa48c44aabd3636153aeda94a8b431
Chen Cen <arrow.cen@gmail.com>
2016-10-30 21:34:24 -0700

    Initial Commit

---
+++ DropIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropIt
+//
+//  Created by Chen Cen on 10/30/16.
+//  Copyright © 2016 Chen Cen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
